5ad5840b36c58d96c5a7035f7fd261b504a23c7d
Parker Lazar <plazar@emich.edu>
2021-04-11 13:26:58 -0400

    player can now attack and take damage

---
+++ Assets/Scripts/player_stats.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_stats : MonoBehaviour
+{
+    public int curHealth = 0;
+    public int maxHealth = 100;
+
+    public HealthBar hb;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        curHealth -= damage;
+        hb.SetHealth(curHealth);
+        if(curHealth == 0)
+        {
+            Destroy(player);
+            Time.timeScale = 0;
+        }
+    }
+}
